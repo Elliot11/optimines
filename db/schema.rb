b030cd802747f37717c2a1b77085c9bb7bc44f7a
Elliot11 <elliot.khoo@gmail.com>
2014-05-10 18:40:34 +0800

    added descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510040934) do
+ActiveRecord::Schema.define(version: 20140510035456) do
 
   create_table "goldmeasures", force: true do |t|
     t.integer  "time"
